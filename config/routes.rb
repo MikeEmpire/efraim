ed69871e832f15e2d4c661643dec2325650cc43c
Michael Olie <aolie94@gmail.com>
2018-05-17 09:00:29 -0700

    added about page and contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
+  get '/about', to: 'visitors#about'
+  get '/contact', to: 'visitors#contact'
 end
